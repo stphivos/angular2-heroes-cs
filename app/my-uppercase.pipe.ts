a1972359d50fd434ee4f06728bed06d2f1fffcbf
Phivos Stylianides <stphivos@gmail.com>
2016-04-21 03:05:01 +0300

    Add .travis.yml

---
+++ app/my-uppercase.pipe.ts
@@ -0,0 +1,8 @@
+import {Pipe, PipeTransform} from 'angular2/core';
+
+@Pipe({name: 'my-uppercase'})
+export class MyUppercasePipe implements PipeTransform {
+    transform(value:string) {
+        return value.toUpperCase();
+    }
+}
